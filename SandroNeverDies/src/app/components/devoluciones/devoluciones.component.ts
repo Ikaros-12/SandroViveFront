a34491437436ff44f83bb6b010f07a683bf08047
eamaya1988 <eamaya1988@gmail.com>
2024-11-09 10:23:51 -0300

    solicitud devoluciones front
    actualizacion formulario de solicitud de devoluciones front

--- SandroNeverDies/src/app/components/devoluciones/devoluciones.component.ts
+++ SandroNeverDies/src/app/components/devoluciones/devoluciones.component.ts
@@ -7,6 +7,19 @@ import { Component } from '@angular/core';
 })
 export class DevolucionesComponent {
 
+   /* // Propiedades para vincular con [(ngModel)]
+   pedido: string = '';
+   producto: string = '';
+   motivo: string = '';
+ 
+   // Arreglos de opciones
+   pedidos: string[] = ['Pedido 1', 'Pedido 2', 'Pedido 3'];
+   productos: string[] = ['Producto A', 'Producto B', 'Producto C'];
+   motivos: string[] = ['Motivo 1', 'Motivo 2', 'Motivo 3'];
+ 
+   // Constructor
+   constructor() { } */
+
    // Variables para almacenar los valores de los campos
    nombre: string = '';
    telefono: string = '';
